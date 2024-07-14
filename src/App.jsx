2380f62d229d7fc350f975091c681da744dd0405
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-14 22:19:23 +0300

    dashboard

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+// import Login from "./components/Login"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './component/Login'
+import Dashboard from './component/Dashboard'
+import Home from './component/Home'
+import Employee from './component/Employee'
+import Category from './component/Category'
+import Profile from './component/Profile'
+import AddCategory from './component/AddCategory'
+import AddEmployee from './component/AddEmployee'
+
+function App() {
+
+
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path='/adminlogin' element={<Login/>}></Route>
+    <Route path='/dashboard' element={
+          <Dashboard /> }></Route>
+        <Route path='' element={<Home />}></Route>
+        <Route path='/dashboard/employee' element={<Employee />}></Route>
+        <Route path='/dashboard/category' element={<Category />}></Route>
+        <Route path='/dashboard/profile' element={<Profile />}></Route>
+        <Route path='/dashboard/add_category' element={<AddCategory />}></Route>
+        <Route path='/dashboard/add_employee' element={<AddEmployee />}></Route>
+   
+  </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
