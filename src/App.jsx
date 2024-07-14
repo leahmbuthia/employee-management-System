492e2743022f6f75c9414de07fcaf72a5605036e
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-14 23:16:53 +0300

    fixing Login error

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,9 @@ function App() {
   <BrowserRouter>
   <Routes>
     <Route path='/adminlogin' element={<Login/>}></Route>
-    <Route path='/dashboard' element={
-          <Dashboard /> }></Route>
-        <Route path='' element={<Home />}></Route>
-        <Route path='/dashboard/employee' element={<Employee />}></Route>
+    <Route path='/dashboard' element={<Dashboard />}></Route>
+    <Route path='' element={<Home />}></Route>
+    <Route path='/dashboard/employee' element={<Employee />}></Route>
         <Route path='/dashboard/category' element={<Category />}></Route>
         <Route path='/dashboard/profile' element={<Profile />}></Route>
         <Route path='/dashboard/add_category' element={<AddCategory />}></Route>
